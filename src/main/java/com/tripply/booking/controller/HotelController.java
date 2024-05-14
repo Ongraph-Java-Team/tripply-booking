b541f7a72d324e1c282eb07a386a0da36128db21
Jatin Suthar <jatin.suthar@ongraph.com>
2024-05-14 15:45:34 +0530

    Organized imports

--- src/main/java/com/tripply/booking/controller/HotelController.java
+++ src/main/java/com/tripply/booking/controller/HotelController.java
@@ -9,7 +9,6 @@ import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
