21bf1de18e0f04323390e6c3a04e739ee12aa63a
Satwik-Bhardwaj <satwikbhardwaj123@gmail.com>
2024-05-20 18:41:21 +0530

    Added pagination API for getAllHotels

--- src/main/java/com/tripply/booking/repository/HotelRepository.java
+++ src/main/java/com/tripply/booking/repository/HotelRepository.java
@@ -1,6 +1,9 @@
 package com.tripply.booking.repository;
 
 import com.tripply.booking.entity.Hotel;
+import com.tripply.booking.model.response.HotelResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,6 @@ import java.util.UUID;
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, UUID>{
 
+
+    Page<HotelResponse> findAllByFilters(Pageable pageable);
 }
--- src/main/java/com/tripply/booking/service/Impl/HotelServiceImpl.java
+++ src/main/java/com/tripply/booking/service/Impl/HotelServiceImpl.java
@@ -23,6 +23,10 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -94,6 +98,12 @@ public class HotelServiceImpl implements HotelService {
         response.setData(hotelResponse);
         log.info("End HotelService getHotelById() with id: {}", hotelId);
         return response;
+    }
+
+    @Override
+    public ResponseModel<Page<HotelResponse>> getAllHotels(String sortBy, String sortOrder, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.fromString(sortOrder), sortBy));
+        Page<HotelResponse> hotelResponses = hotelRepository.findAllByFilters(pageable);
     }
 
     private InviteRequest createHotelInviteRequest(HotelRequest hotelRequest) {
