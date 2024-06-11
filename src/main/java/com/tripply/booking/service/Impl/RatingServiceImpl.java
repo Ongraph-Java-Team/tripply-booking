5778f27ffb96cb13d543fff3d2cb2bf673ecf39e
SaniyaKumawat123 <saniya.kumawat@ongraph.com>
2024-06-11 10:28:07 +0530

    Removed unused imports

--- src/main/java/com/tripply/booking/service/Impl/RatingServiceImpl.java
+++ src/main/java/com/tripply/booking/service/Impl/RatingServiceImpl.java
@@ -24,7 +24,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
-import org.springframework.orm.hibernate5.SpringSessionContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
